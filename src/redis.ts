9eab5f9fb8ab55800194d3c16a6a91450af42599
Jack Allen <jackallensemail+git@gmail.com>
2019-12-18 14:24:08 +0000

    update-url

--- src/redis.ts
+++ src/redis.ts
@@ -1,6 +1,6 @@
 import redis from "redis";
 
-const { REDIS_URL = 'http://localhost:6379' } = process.env;
+const { REDIS_URL = 'redis://127.0.0.1:6379' } = process.env;
 const client = redis.createClient(REDIS_URL);
 
 export const connect = () => {
